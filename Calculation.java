9c3243f91f2c0d71028f8931a1b2008a45e20355
AnkitSingh35 <ankitsingh123p@gmail.com>
2026-06-05 14:32:41 +0530

    Basic Calculation

---
+++ Calculation.java
@@ -0,0 +1,19 @@
+public class Calculation {
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static int mul(int a, int b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        int add = sum(a, b);
+
+        System.out.println("Sum is : " + add);
+        System.out.println("Multiplication is : " + mul(add, b));
+    }
+}
